2d3410d28fc02fce05eb4dbba69781e631768584
Mingalev Mikhail <mingalevme@gmail.com>
2016-09-07 12:50:32 +0300

    Added Filesystem::fitDirIntoSize

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -2,6 +2,9 @@
 
 namespace Mingalevme\Utils;
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
 class Filesystem
 {
     public static function mkdir(string $pathname, int $mode = 0777, string $user = null, string $group = null, resource $context = null)
@@ -47,5 +50,57 @@ class Filesystem
         }
         
         return rmdir($pathname);
+    }
+    
+    /**
+     * Truncate directory to specified size in bytes by step-by-step deleting last accessed files
+     * 
+     * @param string $pathname
+     * @param int $size
+     * @todo add support for human readable sizes: K, M, G, T, P, E, Z, Y (power 1024) or KB, MB, … (power 1000).
+     * @return boolean
+     */
+    public static function fitDirIntoSize(string $pathname, int $size)
+    {
+        $currentSize = intval(static::runConsoleCommand("du -sb {$pathname}"));
+        
+        if ($currentSize < $size) {
+            return true;
+        }
+        
+        while (count($data = static::getNextFilesForFitDirIntoSize($pathname)) > 0) {
+            foreach ($data as $fileData) {
+                list($_, $filesize, $filename) = explode(' ', $fileData, 3);
+                unlink($filename);
+                $currentSize = $currentSize - (int) $filesize;
+                if ($currentSize < $size) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    protected static function getNextFilesForFitDirIntoSize(string $pathname)
+    {
+        $data = explode(\PHP_EOL, static::runConsoleCommand("find {$pathname} -type f -printf \"%T@ %s %p\n\" | sort -n | head -n1000"));
+        unset($data[count($data) - 1]);
+        return $data;
+    }
+
+    protected static function runConsoleCommand($command)
+    {
+        $process = new Process($command);
+        
+        $process->run();
+        
+        if ($process->isSuccessful() === false) {
+            throw new ProcessFailedException($process);
+        }
+        
+        $output = $process->getOutput();
+        
+        return $output;
     }
 }
