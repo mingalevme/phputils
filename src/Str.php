92ed34a5958c68efa6bb5eff51f5d69dcabc5242
Mingalev Mikhail <mingalevme@gmail.com>
2017-02-22 14:52:08 +0300

    Str::random() now is safe

--- src/Str.php
+++ src/Str.php
@@ -4,16 +4,18 @@ namespace Mingalevme\Utils;
 
 class Str extends \Illuminate\Support\Arr
 {
-    public static function random($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    public static function random($length)
     {
-        $str = '';
-        
-        $max = \mb_strlen($keyspace, '8bit') - 1;
-        
-        for ($i = 0; $i < $length; ++$i) {
-            $str .= $keyspace[\rand(0, $max)];
+        if (function_exists('\random_bytes')) {
+            $str = \bin2hex(\random_bytes(($length & 1 ? $length + 1 : $length)/2));
+        } elseif (\function_exists('\openssl_random_pseudo_bytes')) {
+            $str = \bin2hex(\openssl_random_pseudo_bytes(($length & 1 ? $length + 1 : $length)/2));
+        } elseif (\function_exists('\mcrypt_create_iv')) {
+            $str = \bin2hex(\mcrypt_create_iv($length, \MCRYPT_DEV_URANDOM));
+        } else {
+            $str = \str_shuffle(\substr(\str_repeat(\md5(\mt_rand()), 2 + $length/32), 0, $length));
         }
         
-        return $str;
+        return \strlen($str) > $length ? substr($str, 0, $length) : $str;
     }
 }
