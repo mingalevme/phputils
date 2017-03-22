6c6e9a3bb84000bf62e9f9b8f6b09d890812d717
Mingalev Mikhail <mingalevme@gmail.com>
2017-03-22 11:31:34 +0300

    Added Str::strpos() (and short alias - Str::pos) supported array is a  and derivative of Str::strpos() - Str::contains() - only check if  contanins (s)

--- src/Str.php
+++ src/Str.php
@@ -8,6 +8,68 @@ class Str
     const UPPER = 'upper';
     
     /**
+     * (PHP 4, PHP 5, PHP 7)<br/>
+     * Find the position of the first occurrence of a substring (or first
+     * of substring if <i>needle</i> is an array) in a string
+     * @link http://php.net/manual/en/function.strpos.php
+     * @param string $haystack <p>
+     * The string to search in.
+     * </p>
+     * @param mixed $needle <p>
+     * If <i>needle</i> is not a string or an array, it is converted
+     * to an integer and applied as the ordinal value of a character.
+     * </p>
+     * @param int $offset [optional] <p>
+     * If specified, search will start this number of characters counted from
+     * the beginning of the string. Unlike <b>strrpos</b> and
+     * <b>strripos</b>, the offset cannot be negative.
+     * </p>
+     * @return mixed the position of where the needle exists relative to the beginning of
+     * the <i>haystack</i> string (independent of offset).
+     * Also note that string positions start at 0, and not 1.
+     * </p>
+     * <p>
+     * Returns <b>false</b> if the needle was not found.
+     */
+    public static function strpos($haystack, $needle, $offset = 0)
+    {
+        foreach ((array) $needle as $substr) {
+            if (false !== ($index = \strpos($haystack, $substr, $offset))) {
+                return $index;
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Alias of <strong>static::strpos</strong>
+     * 
+     * @param string $haystack
+     * @param mixed $needle string or array of strings
+     * @param int $offset
+     * @return bool
+     */
+    public static function pos($haystack, $needle, $offset = 0)
+    {
+        return static::strpos($haystack, $needle, $offset);
+    }
+    
+    /**
+     * Check if <strong>$haystack</strong> contains <strong>$needle</strong>
+     * or one of <strong>$needle</strong> if $needle is an array
+     * 
+     * @param string $haystack
+     * @param mixed $needle string or array of strings
+     * @param int $offset
+     * @return bool
+     */
+    public static function contains($haystack, $needle, $offset = 0)
+    {
+        return static::strpos($haystack, $needle, $offset) !== false;
+    }
+    
+    /**
      * Generate a safe random string
      * 
      * @param type $length
