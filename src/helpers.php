d846afaf3a410cf73d7d206cd87b10a73fc1c65f
Mingalev Mikhail <mingalevme@gmail.com>
2017-06-14 15:06:40 +0300

    Added aliases: jsond() -> Json::d(), jsone() -> Json::e()

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Mingalevme\Utils\Arr;
+use Mingalevme\Utils\Json;
 
 if (! function_exists('array_compress')) {
     /**
@@ -12,6 +13,32 @@ if (! function_exists('array_compress')) {
     function array_compress(array $array)
     {
         return Arr::compress($array);
+    }
+}
+
+if (! function_exists('jsone')) {
+    /**
+     * \Mingalevme\Utils\Json::encode function alias
+     *
+     * @param  mixed $data
+     * @return string
+     */
+    function jsone($data)
+    {
+        return Json::e($data);
+    }
+}
+
+if (! function_exists('jsond')) {
+    /**
+     * \Mingalevme\Utils\Json::decode function alias
+     *
+     * @param  string $json
+     * @return mixed
+     */
+    function jsond($json)
+    {
+        return Json::d($json);
     }
 }
 
