5aec225bb38b52c70e853941164a45ff2d128a1f
Mingalev Mikhail <mingalevme@gmail.com>
2018-03-15 11:48:41 +0300

    Url::build: *.html? -> *.html

--- src/Url.php
+++ src/Url.php
@@ -106,7 +106,7 @@ class Url
             .((isset($parse_url['host'])) ? $parse_url['host'] : '')
             .((isset($parse_url['port'])) ? ':' . $parse_url['port'] : '')
             .((isset($parse_url['path'])) ? $parse_url['path'] : '')
-            .((isset($parse_url['query'])) ? '?' . $parse_url['query'] : '')
+            .((isset($parse_url['query']) && $parse_url['query']) ? '?' . $parse_url['query'] : '')
             .((isset($parse_url['fragment'])) ? '#' . $parse_url['fragment'] : '')
             ;
     }
